8c956ff2ba06c7bb29f020327b73c8f8dcc66a0c
Adam Seitz <adamseitz98@gmail.com>
2021-06-26 18:34:14 -0400

    Added 5 round rule

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,5 @@
-
+let player_score = 0;
+let computer_score =0;
 function computerPlay(){
     let choice = Math.floor(Math.random() * 4);
     if (choice == 1){
@@ -22,26 +23,42 @@ function singleRound(playerSelection, computerSelection){
         case "rock":
             return "You tie!";
         case "paper":
+            if(player_score != 0){
+                player_score --;
+            }
+            computer_score ++;
             return "You lose! Paper beats Rock!";
         case "scissors":
+            if(computer_score != 0){
+                computer_score --;
+            }
+            player_score ++;
             return "You win! Rock beats Scissors!";  
        }
     }
     else if(playerSelection == "paper"){
         switch(computerSelection){
             case "rock":
+                
+                player_score ++;
                 return "You win! Paper beats Rock!";
             case "paper":
                 return "You tie!";
             case "scissors":
+                
+                computer_score ++;
                 return "You lose! Scissors beat Paper!";  
            }
         }
     else{
         switch(computerSelection){
             case "rock":
+                
+                computer_score ++;
                 return "You Lose! Rock beats Scissors!";
             case "paper":
+                
+                player_score ++;
                 return "You win! Scissors beats Paper";
             case "scissors":
                 return "You tie!";  
@@ -55,16 +72,45 @@ function game(){
         const results = document.querySelector('#results');
         const display_player = document.querySelector('#player-choice');
         const display_computer = document.querySelector('#computer-choice');
+        const player_points = document.querySelector('#player-score');
+        const computer_points = document.querySelector('#computer-score');
         buttons.forEach((button) => {
             
             button.addEventListener('click', () =>{
+                
                 let computer_choice = computerPlay();
                 player_choice = button.id;
                 display_player.textContent = `Your choice: ${player_choice}`;
                 display_computer.textContent = `Computer's choice: ${computer_choice}`;
                 results.textContent = singleRound(player_choice, 
                     computer_choice);
-
+                player_points.textContent = `Your Score: ${player_score}`;
+                computer_points.textContent = `Computer Score: ${computer_score}`;    
+                if (computer_score == 5){
+                    
+                    setTimeout( () => {alert("You've lost!");
+                        player_points.textContent = `Your Score: 0`;
+                        computer_points.textContent = `Computer Score: 0`;
+                        computer_score = 0;
+                        player_score = 0;
+                        results.textContent = "Results Per Hand Show Here";
+                        }, 200);
+                    
+                    
+                }
+                if (player_score ==5){
+                    
+                    setTimeout( () => {alert("You've won!");
+                        player_points.textContent = `Your Score: 0`;
+                        computer_points.textContent = `Computer Score: 0`;
+                        computer_score = 0;
+                        player_score = 0;
+                        results.textContent = "Results Per Hand Show Here";
+                        }, 200);
+                    
+                    
+                }
+                
             });
             
         });  
